8240e78d3fdb0b679f0441c38928400e253e95ac
So Murota <so@elephantnode.com>
2024-06-11 16:26:03 +0900

    set direct local path at command path

--- lib/wordmove/sql_adapter/wpcli.rb
+++ lib/wordmove/sql_adapter/wpcli.rb
@@ -16,7 +16,8 @@ module Wordmove
         end
 
         opts = [
-          "--path=#{cli_config_path}",
+          # "--path=#{cli_config_path}",
+          "--path=#{@local_path}",
           from,
           to,
           "--quiet",
